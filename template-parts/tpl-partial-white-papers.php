075b1cab9b330c36de719ce7c0338aefa68b0b23
Nadav.Avidan <nadav.avidan@convertro.com>
2014-01-05 13:26:17 +0200

    white papers

--- template-parts/tpl-partial-white-papers.php
+++ template-parts/tpl-partial-white-papers.php
@@ -4,12 +4,8 @@
 		<div class="section-title row">
 			<h2 class="title col-md-9"><?php _e('White Papers'); ?></h2>
 			<div class="col-md-3 text-right">
-				<a class="demo-link orange" href="<?php echo get_post_type_archive_link('white_paper'); ?>">
-					<?php _e("See all white papers"); ?>
-					<svg class="tip" height="26" width="14">
-		        		<polygon points="0,27 0,27 0,0 0,0 10.084,13.213"/>
-		    		</svg>
-				</a>
+				<?php echo get_demo_link('orange', get_post_type_archive_link('white_paper'), __('See all white papers')); ?>
+
 			</div>
 		</div>
 		<?php
