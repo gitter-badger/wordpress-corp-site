7b4fc562237c6ce3d8d258260f9170314363e2fa
Nadav.Avidan <nadav.avidan@convertro.com>
2014-01-05 13:26:51 +0200

    delete unnecessary templates

--- home.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-<div id="container">
-	<div id="content">
-		<p>Convertro Home</p>
-		<?php get_template_part( 'allPostsLoop' ); ?>
-	</div>
-	<?php get_sidebar(); ?>
-</div>
-<?php get_footer(); ?>
