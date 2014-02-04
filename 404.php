cb58713f724a803c33083e651b99f586cdab01c9
Nadav.Avidan <nadav.avidan@convertro.com>
2014-02-04 08:57:19 +0200

    Add 404 page template

--- 404.php
+++ 404.php
@@ -1,10 +1,45 @@
-<?php get_header(); ?>
-	<h1 class="page-title">
-		<?php _e( '404: Page Not Found', 'convertro' ); ?>
-	</h1>
-	<div>
-		<p><?php _e( 'We are terribly sorry, but the URL you typed no longer exists. It might have been moved or deleted. Try searching the site:', 'convertro' ); ?></p>
-		<?php get_search_form(); ?>
+<?php
+/*
+Template Name: 404
+*/
+get_header(); ?>
+<div id="content" class="single-post-page">
+<div id="404-page" class="theme_bg_darker section">
+	<div class="container">
+		<div class="row">
+		</div>
 	</div>
-<?php get_sidebar(); ?>
+	<div class="section-title">
+		<div class="container">
+			<div class="col-xs-10 content-404 box-center text-center">
+				<div class="layer-1">
+					404
+				</div>
+				<div class="layer-2">
+					<div class="huge-text">
+						SORRY
+					</div>
+					<div class="layer-3">
+						<div class="block">
+							<span class="medium-text">NEEDLESS TO </span>
+							<span class="big-text">SAY...</span>
+							<span class="small-text rotate-small">BUT</span>
+						</div>
+						<div class="block text-section-3-4">
+							THE PAGE THAT <span class="text-dimmed">YOU WERE LOOKING FOR</span> IS <span class="emphasis">UNAVAILABLE</span>
+						</div>
+					</div>
+
+
+				</div>
+
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="row ">
+		</div>
+	</div>
+</div>
+</div>
 <?php get_footer(); ?>
